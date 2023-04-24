e4d76eab4a1d78fb1a7c237170793e35296029b1
PhongNguyenTrung <90634242+PhongNguyenTrung@users.noreply.github.com>
2023-04-24 18:07:04 +0700

    Using Grape

--- user-api/app/api/base.rb
+++ user-api/app/api/base.rb
@@ -1,5 +1,7 @@
-module API
-  class Base < Grape::API
-    mount V1::UserApi
-  end
+# This Base class is responsible for generating all API from multiple versions
+class Base < Grape::API
+  format :json
+  prefix :api
+
+  mount V1::UserApi
 end
--- user-api/app/api/v1/user_api.rb
+++ user-api/app/api/v1/user_api.rb
@@ -1,16 +1,66 @@
-# This modlue is responsible for
+# This module is responsible for User API with version 1.
 module V1
-  # This modlue is responsible for User API
-  class UserApi < Graph::API
-    version 'v1', using: :path
+  # This class is responsible for User API with version 1.
+  class UserApi < Grape::API
+    helpers JwtToken
+    helpers do
+      def authenticate_user
+        header = request.headers['Authorization']
+        header = header.split.last if header
+        begin
+          @decoded = jwt_decode(header)
+          @current_user = User.find(@decoded[:user_id])
+        rescue ActiveRecord::RecordNotFound, JWT::DecodeError
+          error!('Unauthorized', 401)
+        end
+      end
+    end
 
+    version 'v1', using: :path
     resource :users do
       desc 'Get all users'
-
-      get do
+      get '/get_all_users' do
         @users = User.all
+        present @users, with: Entities::V1::UserFormat
+      end
 
-        present @user, with: Entities::V1::UserFormat
+      desc 'Sign up user'
+      params do
+        requires :name, type: String, allow_blank: false
+        optional :phone, type: String, allow_blank: false
+        optional :address, type: String, allow_blank: false
+        requires :email, type: String, allow_blank: false, regexp: URI::MailTo::EMAIL_REGEXP
+        requires :password, type: String, allow_blank: false
+        requires :password_confirmation, type: String, allow_blank: false, same_as: :password
+      end
+      post '/signup' do
+        user = User.new(declared(params))
+        error!('400 Bad_Request', 400) unless user.save
+        present user, with: Entities::V1::UserFormat
+      end
+
+      desc 'Login User'
+      params do
+        requires :email, type: String, allow_blank: false, regexp: URI::MailTo::EMAIL_REGEXP
+        requires :password, type: String, allow_blank: false
+      end
+      post '/login' do
+        user = User.find_by(email: params[:email].downcase)
+        if user&.authenticate(params[:password])
+          payload = { user_id: user.id }
+          token = jwt_encode(payload)
+          present token:
+        else
+          error!('401 Unauthorized', 401)
+        end
+      end
+
+      desc 'Get user with bearer token'
+      before do
+        authenticate_user
+      end
+      get '/current_user' do
+        present @current_user, with: Entities::V1::UserFormat
       end
     end
   end
