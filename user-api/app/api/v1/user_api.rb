6a34ef24bc4af2b89b1e1387940031ea4f2530d9
PhongNguyenTrung <90634242+PhongNguyenTrung@users.noreply.github.com>
2023-04-25 10:58:59 +0700

    Apply rubocop

--- user-api/app/api/v1/user_api.rb
+++ user-api/app/api/v1/user_api.rb
@@ -11,48 +11,18 @@ module V1
           @decoded = jwt_decode(header)
           @current_user = User.find(@decoded[:user_id])
         rescue ActiveRecord::RecordNotFound, JWT::DecodeError
-          error!('Unauthorized', 401)
+          error!('401 Unauthorized', 401)
         end
       end
     end
 
     version 'v1', using: :path
-    resource :users do
+
+    resources :users do
       desc 'Get all users'
-      get '/get_all_users' do
+      get do
         @users = User.all
         present @users, with: Entities::V1::UserFormat
-      end
-
-      desc 'Sign up user'
-      params do
-        requires :name, type: String, allow_blank: false
-        optional :phone, type: String, allow_blank: false
-        optional :address, type: String, allow_blank: false
-        requires :email, type: String, allow_blank: false, regexp: URI::MailTo::EMAIL_REGEXP
-        requires :password, type: String, allow_blank: false
-        requires :password_confirmation, type: String, allow_blank: false, same_as: :password
-      end
-      post '/signup' do
-        user = User.new(declared(params))
-        error!('400 Bad_Request', 400) unless user.save
-        present user, with: Entities::V1::UserFormat
-      end
-
-      desc 'Login User'
-      params do
-        requires :email, type: String, allow_blank: false, regexp: URI::MailTo::EMAIL_REGEXP
-        requires :password, type: String, allow_blank: false
-      end
-      post '/login' do
-        user = User.find_by(email: params[:email].downcase)
-        if user&.authenticate(params[:password])
-          payload = { user_id: user.id }
-          token = jwt_encode(payload)
-          present token:
-        else
-          error!('401 Unauthorized', 401)
-        end
       end
 
       desc 'Get user with bearer token'
@@ -61,6 +31,37 @@ module V1
       end
       get '/current_user' do
         present @current_user, with: Entities::V1::UserFormat
+      end
+    end
+
+    desc 'Sign up user'
+    params do
+      requires :name, type: String, allow_blank: false
+      optional :phone, type: String, allow_blank: false
+      optional :address, type: String, allow_blank: false
+      requires :email, type: String, allow_blank: false, regexp: URI::MailTo::EMAIL_REGEXP
+      requires :password, type: String, allow_blank: false
+      requires :password_confirmation, type: String, allow_blank: false, same_as: :password
+    end
+    post '/signup' do
+      user = User.new(params)
+      error!("400 Bad_Request: #{user.errors.full_messages.join(',')}", 400) unless user.save
+      present user, with: Entities::V1::UserFormat
+    end
+
+    desc 'Login User'
+    params do
+      requires :email, type: String, allow_blank: false, regexp: URI::MailTo::EMAIL_REGEXP
+      requires :password, type: String, allow_blank: false
+    end
+    post '/login' do
+      user = User.find_by(email: params[:email].downcase)
+      if user&.authenticate(params[:password])
+        payload = { user_id: user.id }
+        token = jwt_encode(payload)
+        present token:
+      else
+        error!('401 Unauthorized', 401)
       end
     end
   end
